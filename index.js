f55398b81bd982a7ee29eaf3864871c6ae3c7ca6
191prajjwal <191sprajjwal@gmail.com>
2024-10-18 19:19:11 +0530

    created userRoutes utils and login register done

--- index.js
+++ index.js
@@ -2,12 +2,14 @@ const express = require("express")
 const mongoose=require("mongoose")
 const dotenv=require("dotenv")
 
-const incommingReqLogger= require("./middlewares/logger")
+const userRoutes= require("./routes/userRoutes")
 
 dotenv.config()
 const app = express()
-
 const PORT= process.env.PORT || 4005
+
+const incommingReqLogger= require("./middlewares/logger")
+app.use(express.json())
 
 app.use(incommingReqLogger)
 
@@ -15,6 +17,8 @@ app.use(incommingReqLogger)
 app.get("/",(req,res)=>{
     res.send("Working fine")
 })
+
+app.use("/api/v1/users",userRoutes)
 
 
 app.listen(PORT,()=>{
---
+++ routes/userRoutes.js
@@ -0,0 +1,105 @@
+const express= require("express")
+const router = express.Router()
+const User= require("../models/userModel")
+const isValidEmail= require("../utils/helper")
+const bcrypt= require("bcrypt")
+const jwt= require("jsonwebtoken")
+
+
+router.post("/register",async(req,res)=>{
+
+    try{
+        const{name,email,password,confirmPassword}=req.body;
+
+
+
+        if(!name || !email || !password || !confirmPassword)
+        {
+
+            return res.status(400).json({
+               success:false, 
+               status:400,
+               message:"All fields are necessary." 
+            })
+        }
+
+        if(password.length<8 || confirmPassword!==password)
+        {
+            return res.status(400).json({
+                message:"Password must be atleast 8 characters long and should match to confirm password field "
+            })
+        }
+
+        if(!isValidEmail(email))
+        {
+            return res.status(400).json({success:false,message:"Invalid email address"})
+        }
+
+        const existingUser = await User.findOne({ email });
+        if (existingUser) {
+            return res.status(409).json({ success:false,message: 'User already exists' });
+        }
+
+        const hashedPassword = await bcrypt.hash(password, 10);
+
+
+        const user = new User({
+            name,
+            email,
+            password: hashedPassword 
+        });
+
+            await user.save()
+            res.status(201).json({success:true,message:"User successfully registered"})
+    }
+
+catch(err){
+
+    res.status(500).json({success:false,message:"Registration unsuccessful", error:err.message})
+
+}
+
+})
+
+
+router.post("/login",async(req,res)=>{
+
+    try{
+
+        const {email,password}= req.body
+        const user= await User.findOne({email})
+
+        if(!user){
+
+          return res.status(404).json({success:false, message:"Invalid email or password"})
+        }
+
+        const isPasswordTrue= await bcrypt.compare(password,user.password)
+
+        // console.log(isPasswordTrue)
+
+        if(!isPasswordTrue)
+        {
+          return res.status(401).json({success:false,message:"Invalid email or password"})
+        }
+
+        const token = jwt.sign({userId:user._id.toString()},process.env.JWT_SECRET_KEY)
+
+        return res.status(200).json({success:true,message:"Login successful",token,userId:user._id.toString(),name:user.name})
+
+
+    }
+
+
+    catch(err)
+    {    console.log(err)
+        res.status(500).json({message:"Login unsuccessful",error:err.message})
+    }
+
+})
+
+
+
+
+
+module.exports= router
