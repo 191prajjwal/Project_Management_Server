81165bf50dd3dde35b48364d0d579f061b6df5ab
191prajjwal <191sprajjwal@gmail.com>
2024-10-23 10:03:30 +0530

    updated routes

--- routes/taskRoutes.js
+++ routes/taskRoutes.js
@@ -8,13 +8,13 @@ const authMiddleware= require("../middlewares/auth")
 router.post("/create",authMiddleware,async (req,res)=>{
 
     try {
-        const {title,priority,checklist,dueDate,status,userId}= req.body
+        const {title,priority,checklist,dueDate,board,userId}= req.body
 
-    const task= new Task({title,priority,checklist,dueDate,status,userId})
+    const newTask= new Task({title,priority,checklist,dueDate,board,userId})
 
-    await task.save()
+    await newTask.save()
 
-    res.status(201).json({success:true,message:"Task successfully created",task:task})
+    res.status(201).json({success:true,message:"Task successfully created",task:newTask})
     } 
     catch (err) {
 
@@ -50,14 +50,14 @@ router.put("/update/:taskId",authMiddleware,async(req,res)=>{
         const{taskId}=req.params
         const{title,priority,checklist,dueDate,userId}=req.body
 
-        const task= await Task.findByIdAndUpdate({_id:taskId},{title,priority,checklist,dueDate,userId},{new:true})
+        const updatedTask= await Task.findByIdAndUpdate({_id:taskId},{title,priority,checklist,dueDate,userId},{new:true})
 
-        if(!task)
+        if(!updatedTask)
         {
-            return res.status(404).json({success:false,message:"Task not found"})
+            return res.status(404).json({success:false,error:"Task not found"})
         }
 
-        res.status(200).json({success:true,message:"Task successfully updated",task:task})
+        res.status(200).json({success:true,message:"Task successfully updated",task:updatedTask})
 
 
     } catch (err) {
@@ -73,14 +73,14 @@ router.delete("/delete/:taskId",authMiddleware,async(req,res)=>{
 
     try{
         const{taskId}=req.params
-        const task= await Task.findByIdAndDelete({_id:taskId})
+        const deletedTask= await Task.findByIdAndDelete({_id:taskId})
 
-        if(!task)
+        if(!deletedTask)
         {
             return res.status(404).json({success:false,message:"Task not found"})
         }
 
-        res.status(200).json({success:true,message:"Task deleted successfully",task:task})
+        res.status(200).json({success:true,message:"Task deleted successfully",task:deletedTask})
     }
 
     catch(err)
@@ -95,58 +95,47 @@ router.delete("/delete/:taskId",authMiddleware,async(req,res)=>{
 
 router.post("/filter",authMiddleware,async(req,res)=>{
 
-    try{
-        const{userId,boardDate}=req.body
-
-        let startDate,endDate
-
-        if(boardDate==='today')
-        {
-            endDate= new Date()
-            startDate= new Date(endDate)
-            startDate.setHours(startDate.getHours()-24)
+    try {
+        const { userId, boardDate } = req.body;
+  
+        let startDate, endDate;
+        if (boardDate === 'today') {
+          endDate = new Date();
+          startDate = new Date(endDate);
+          startDate.setHours(startDate.getHours() - 24);
+        } else if (boardDate === 'thisWeek') {
+          const today = new Date();
+          const dayOfWeek = today.getDay();
+          startDate = new Date(today);
+          startDate.setDate(today.getDate() - dayOfWeek);
+          endDate = new Date(startDate);
+          endDate.setDate(startDate.getDate() + 6);
+        } else if (boardDate === 'thisMonth') {
+          const today = new Date();
+          startDate = new Date(today.getFullYear(), today.getMonth(), 1);
+          endDate = new Date(today.getFullYear(), today.getMonth() + 1, 0);
         }
 
-        else if(boardDate==='thisWeek')
-        {
-            const today= new Date()
-            const day= today.getDay()
-            startDate= new Date(today)
-            startDate.setDate(today.getDate()-day)
-            endDate= new Date(startDate)
-            endDate.setDate(startDate.getDate()+6)
-        }
-
-        else if(boardDate==="thisMonth")
-        {
-            const today= new Date()
-            startDate= new Date(today.getFullYear(),today.getMonth(),1)
-            endDate= new Date(today.getFullYear(),today.getMonth()+1,0)
-        }
-
-
-        const filter= await Task.find({userId,createdDate:{$gte:startDate,$lte:endDate}})
-
-        res.status(200).json({filter})
-    }
-    catch(err)
-    {
-        res.status(500).json({error:"Error fetching the tasks"})
-    }
+        const tasksToDo = await Task.find({ userId, createdDate: { $gte: startDate, $lte: endDate } });
+        res.status(200).json({ tasksToDo });
+      } catch (error) {
+        res.status(500).json({ error: 'Error retrieving tasks' });
+      }
+    
 
 })
 
 
-router.post("/update/status",authMiddleware,async(req,res)=>{
+router.post("/update/board",authMiddleware,async(req,res)=>{
     try{
-        const {taskId,status}=req.body
-        const task= await Task.findByIdAndUpdate(taskId,{status:status},{new:true})
+        const {taskId,newBoard}=req.body
+        const updatedTask= await Task.findByIdAndUpdate(taskId,{board:newBoard},{new:true})
 
-        res.status(200).json({success:true,message:"status updated successfully", task:task})
+        res.status(200).json({success:true,message:"Board updated successfully", task:updatedTask})
     }
     catch(err)
     {
-            res.status(500).json({error:"Error updating status"})
+            res.status(500).json({error:"Error updating board"})
     }
 })
 
@@ -155,7 +144,7 @@ router.post("/update/checklist",authMiddleware,async (req,res)=>{
 
     try {
 
-        const {taskId,checklistItemId,isChecked}=req.body
+        const {taskId,checklistItemId,completed}=req.body
         const task= await Task.findById(taskId)
         if(!task)
         {
@@ -169,9 +158,9 @@ router.post("/update/checklist",authMiddleware,async (req,res)=>{
             return res.status(404).json({success:false,message:"checklist item not found"})
         }
 
-        if((checklistItem.isChecked!==isChecked) && (typeof isChecked=== 'boolean'))
+        if((checklistItem.completed!==completed) && (typeof completed=== 'boolean'))
         {
-            checklistItem.isChecked=isChecked
+            checklistItem.completed=completed
             await task.save()
         }
 
